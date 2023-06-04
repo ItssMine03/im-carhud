c2c897f4cc4e4e1255dc9c32d9c9c0a4d4d75bb4
ItssMine03 <68471264+ItssMine03@users.noreply.github.com>
2023-06-04 20:10:47 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'common'
+
+
+author 'Nightt#8899 || Thanks Jaareet <3'
+
+description 'Carhud Modificado por ItssMine03#9034'
+
+description 'im-carhud'
+
+client_scripts {
+    'client/client.lua'
+}
+
+ui_page {
+    'ui/ui.html'
+}
+
+files {
+    '**/**/**/**/**/**/**/**/*.*'
+}
